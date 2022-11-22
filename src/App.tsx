5b035ba00a798c6b8325eb2c90bedaf1fc5d43b4
Rostislav Klein <rostaklein@gmail.com>
2022-11-22 19:30:34 +0100

    basic container and title styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container mx-auto py-4">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+    <div className="container mx-auto pb-4 pt-4 sm:pt-12 px-4">
+      <h1 className="sm:text-3xl text-xl font-bold text-center sm:text-left">
+        💰 Currency converter
+      </h1>
     </div>
   );
 }
